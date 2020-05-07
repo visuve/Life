c373d13da104f7a0a80ef82d5310c7822aefd263
visuve <veli-matti.visuri@iki.fi>
2020-05-07 23:31:56 +0300

    Fix imports

--- life.py
+++ life.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 
 class GameScene(QGraphicsScene):
