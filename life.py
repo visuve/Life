6235ce71d8308d87ce59cba60c08ef8c21f2b9ed
visuve <veli-matti.visuri@iki.fi>
2020-05-08 21:04:36 +0300

    Calculate neighbours in a different function

--- life.py
+++ life.py
@@ -98,17 +98,15 @@ class GameScene(QGraphicsScene):
                 self.setVisible(x, y, False)
 
         # Clear the alive cells from the array containing states for each cell
-        self.gameField.cells = [[False] * 100 for i in range(150)]
+        self.gameField.reset()
         print("Scene cleared")
 
 
 class GameField:
-    def __init__(self):
-        print("Field created")
 
-        # Create two two-dimensional arrays for the current cells and the cells
-        # which are about to come alive
-        self.cells = [[False] * 100 for i in range(150)]
+    def __init__(self):
+        self.cells = None
+        self.reset()
 
         # A "glider" For testing purposes
         '''
@@ -119,55 +117,63 @@ class GameField:
         self.cells[52][50] = True
         '''
 
+        print("Field created")
+
+    def reset(self):
+        # Create two two-dimensional arrays for the cells
+        self.cells = [[False] * 100 for i in range(150)]
+
     def isAliveAt(self, x, y):
         return self.cells[x][y] == True
 
+    def calculateNeighbours(self, x, y):
+        ''' Check if the cell is alive and then
+            check the count of neighbour of each cell
+            _______________________
+            |x-1,y+1|x,y+1|x+1,y+1|
+            |_______|_____|_______|
+            |x-1,y  |x,y  |x+1,y  |
+            |_______|_____|_______|
+            |x-1,y-1|x,y-1|x+1,y-1|
+            |_______|_____|_______|
+        '''
+
+        # Set the count of neighbours of each cell as zero at start
+        neighbours = 0
+
+        # Top row
+        if (self.cells[x - 1][y + 1]):
+            neighbours += 1
+        if (self.cells[x][y + 1]):
+            neighbours += 1
+        if (self.cells[x + 1][y + 1]):
+            neighbours += 1
+
+        # Middle row
+        if (self.cells[x - 1][y]):
+            neighbours += 1
+        if (self.cells[x + 1][y]):
+            neighbours += 1
+
+        # Bottom row
+        if (self.cells[x - 1][y - 1]):
+            neighbours += 1
+        if (self.cells[x][y - 1]):
+            neighbours += 1
+        if (self.cells[x + 1][y - 1]):
+            neighbours += 1
+
+        return neighbours
+
     def calculateCells(self):
-        # This method contains all the actual game logic
         neighbours = 0
 
         newCells = [[False] * 100 for i in range(150)]
 
         for x in range(1, 149):
             for y in range(1, 99):
-
-                ''' Check if the cell is alive and then
-                    check the count of neighbour of each cell
-                    _______________________
-                    |x-1,y+1|x,y+1|x+1,y+1|
-                    |_______|_____|_______|
-                    |x-1,y  |x,y  |x+1,y  |
-                    |_______|_____|_______|
-                    |x-1,y-1|x,y-1|x+1,y-1|
-                    |_______|_____|_______|
-                '''
-
-                # Set the count of neighbours of each cell as zero at start
-                neighbours = 0
-
-                # Top row
-                if (self.cells[x - 1][y + 1]):
-                    neighbours += 1
-                if (self.cells[x][y + 1]):
-                    neighbours += 1
-                if (self.cells[x + 1][y + 1]):
-                    neighbours += 1
-
-                # Middle row
-                if (self.cells[x - 1][y]):
-                    neighbours += 1
-                if (self.cells[x + 1][y]):
-                    neighbours += 1
-
-                # Bottom row
-                if (self.cells[x - 1][y - 1]):
-                    neighbours += 1
-                if (self.cells[x][y - 1]):
-                    neighbours += 1
-                if (self.cells[x + 1][y - 1]):
-                    neighbours += 1
-
                 # Rules from: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+                neighbours = self.calculateNeighbours(x, y)
 
                 # 1. Any live cell with fewer than two live neighbours dies,
                 # as if by underpopulation.
@@ -192,8 +198,7 @@ class GameField:
                     newCells[x][y] = True
                     continue
 
-        # Set the newborn and killed cells to the array
-        # containing the cells for the game logic
+        # Swap the buffer, i.e. the changed cells to the actual ones
         self.cells = newCells
 
 
@@ -225,9 +230,9 @@ class LifeWindow(QWidget):
         self.pushButtonNew.setDisabled(True)
 
         # Connect pushbutton clicks to their slots
-        self.pushButtonStart.clicked.connect(self.pushButtonStart_clicked)
-        self.pushButtonPause.clicked.connect(self.pushButtonPause_clicked)
-        self.pushButtonNew.clicked.connect(self.pushButtonNew_clicked)
+        self.pushButtonStart.clicked.connect(self.pushButtonStartClicked)
+        self.pushButtonPause.clicked.connect(self.pushButtonPauseClicked)
+        self.pushButtonNew.clicked.connect(self.pushButtonNewClicked)
 
         # Create a gridlaoyt for the pushbuttons and the graphicsview
         gridLayout = QGridLayout(self)
@@ -253,7 +258,7 @@ class LifeWindow(QWidget):
         # Allways upon user resizing the window the aspect ratio is kept at 3:2
         self.graphicsView.fitInView(0, 0, 600, 400, Qt.KeepAspectRatio)
 
-    def pushButtonStart_clicked(self):
+    def pushButtonStartClicked(self):
         # Start the graphicsscenes own timer and start the whole game and its
         # logic
         self.gameScene.timer.start(50)
@@ -263,7 +268,7 @@ class LifeWindow(QWidget):
         self.pushButtonPause.setEnabled(True)
         self.pushButtonNew.setDisabled(True)
 
-    def pushButtonPause_clicked(self):
+    def pushButtonPauseClicked(self):
         # Stop the graphicsscenes own timer to "pause" the game
         self.gameScene.timer.stop()
         print("Timer stopped")
@@ -272,7 +277,7 @@ class LifeWindow(QWidget):
         self.pushButtonPause.setDisabled(True)
         self.pushButtonNew.setEnabled(True)
 
-    def pushButtonNew_clicked(self):
+    def pushButtonNewClicked(self):
         # Stop the timer and call the graphicsscenes method clearScene for a
         # new start
         self.gameScene.timer.stop()
